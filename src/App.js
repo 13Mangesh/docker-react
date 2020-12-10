7cd3319f7aee23aba780f49f34412fa2d1a47c1d
13Mangesh <13mangeshpuri@gmail.com>
2020-12-10 22:38:49 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>First Docker Environment application</h1>
+			<h1>First Docker Environment App</h1>
 		</div>
 	);
 }
